a45c5a7a3f7c2fbec9e4c32504ff79bcb3c4e6fe
Adeesh-bode <bodead@rknec.edu>
2025-07-02 11:56:29 +0530

    fix:on productn image issue

--- feature/Shared/auth/AuthButtons/index.tsx
+++ feature/Shared/auth/AuthButtons/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-"use client"
+"use client";
 // import { signOut } from "next-auth/react";
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
@@ -11,7 +11,7 @@ import { Bell, LogOut } from "lucide-react";
 const AuthButtons = () => {
   // const router = useRouter();
   const { data, status } = useSession(); // session as alias
-  if (status === 'loading') return null;
+  if (status === "loading") return null;
   const session: any = (data as any)?.session;
   // console.log("useSession raw output", useSession());
 
@@ -27,12 +27,26 @@ const AuthButtons = () => {
         </span> */}
         {/* <p className="text-gray-600 " onClick={()=>router.push("/my-profile")}  >{session?.accessToken}</p> */}
         <p className="text-gray-600 ">{session?.user?.name}</p>
-        <span className="relative rounded-full overflow-hidden"   onClick={() => window.open('/my-profile', '_blank')}  >
-          <Image src={session?.user?.profileImage  || '/images/product/fashionx/avatar.png' } alt="Profile Image" width={40} height={40} className="w-10 h-10 aspect-square object-cover" />
+        <span
+          className="relative rounded-full overflow-hidden"
+          onClick={() => window.open("/my-profile", "_blank")}
+        >
+          <Image
+            src={
+              session?.user?.profileImage ||
+              "/images/product/fashionx/avatar.png"
+            }
+            alt="Profile Image"
+            width={40}
+            height={40}
+            className="w-10 h-10 aspect-square object-cover"
+            unoptimized={true}
+            priority
+          />
         </span>
         {/* <p className="text-gray-600 " onClick={()=>router.push("/my-profile")}  >{session?.user?.email}</p> */}
         <span
-          onClick={()=>signOut()}
+          onClick={() => signOut()}
           // href="/api/auth/signout"
           className="flex gap-4 text-red-500 cursor-pointer"
         >
@@ -42,15 +56,22 @@ const AuthButtons = () => {
       </div>
     );
   }
-  return <div className="flex gap-4 items-center" >
-    <Link href="/api/auth/signin" className="flex gap-4 text-black text-lg lg:text-2xl hover:text-green-500 ">
+  return (
+    <div className="flex gap-4 items-center">
+      <Link
+        href="/api/auth/signin"
+        className="flex gap-4 text-black text-lg lg:text-2xl hover:text-green-500 "
+      >
         Log In
-    </Link>
-    <Link href="/signup" className="flex gap-4 bg-green-500 text-white px-3 py-2 rounded-lg text-lg lg:text-xl">
+      </Link>
+      <Link
+        href="/signup"
+        className="flex gap-4 bg-green-500 text-white px-3 py-2 rounded-lg text-lg lg:text-xl"
+      >
         Sign Up
-    </Link>
-
-  </div>;
+      </Link>
+    </div>
+  );
 };
 
 export default AuthButtons;
